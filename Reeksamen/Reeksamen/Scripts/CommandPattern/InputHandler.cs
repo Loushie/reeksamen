eab7b8d78baf5643e4a868096d8f1f1ccf3977e6
Rasmus0008 <Powermine@jubiigames.dk>
2020-07-07 14:45:09 +0200

    Trying to fix this shit

---
+++ Reeksamen/Reeksamen/Scripts/CommandPattern/InputHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Reeksamen.CommandPattern;
+using Reeksamen.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reeksamen
+{
+
+    class InputHandler
+    {
+        private Dictionary<Keys, ICommand> keybinds = new Dictionary<Keys, ICommand>();
+
+        public InputHandler()
+        {
+            keybinds.Add(Keys.D, new MoveCommand(new Vector2(1, 0)));
+            keybinds.Add(Keys.A, new MoveCommand(new Vector2(-1, 0)));
+            keybinds.Add(Keys.W, new MoveCommand(new Vector2(0, -1)));
+            keybinds.Add(Keys.S, new MoveCommand(new Vector2(0, 1)));
+        }
+
+        public void Execute(Player player)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            foreach (Keys key in keybinds.Keys)
+            {
+                if (keyState.IsKeyDown(key))
+                {
+                    keybinds[key].Execute(player);
+                }
+            }
+        }
+
+    }
+}
