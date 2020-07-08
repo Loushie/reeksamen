9cac1536338da2e467acc4f2cfa5396427cbe585
Loushie <57252834+Loushie@users.noreply.github.com>
2020-07-08 14:24:28 +0200

    more db things

---
+++ Reeksamen/Reeksamen/SqliteFramework/Conn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reeksamen.SqliteFramework
+{
+    public static class Conn
+    {
+        public static SQLiteConnection GetCon()
+        {
+            string DatabaseName = "Stats";
+
+            SQLiteConnection con = new SQLiteConnection($"Data Source={DatabaseName}.db; Version=3;New=True;");
+            return con;
+        }
+
+        public static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
